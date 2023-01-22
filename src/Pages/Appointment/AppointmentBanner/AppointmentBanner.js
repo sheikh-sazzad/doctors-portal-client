be927c60021743b300783aad29093270878fda3a
sheikh-sazzad <sheikh15-11045@diu.edu.bd>
2023-01-23 02:19:53 +0600

    modal

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,9 +1,15 @@
-import React from "react";
 import chair from "../../../assets/images/chair.png";
+import { DayPicker } from "react-day-picker";
+import bg from "../../../assets/images/bg.png";
 
-const AppointmentBanner = () => {
+const AppointmentBanner = ({ selectedDate, setSelectedDate }) => {
   return (
-    <header>
+    <header
+      style={{
+        background: `url(${bg})`,
+      }}
+      className="my-6"
+    >
       <div className="hero">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img
@@ -11,7 +17,13 @@ const AppointmentBanner = () => {
             className="max-w-sm rounded-lg shadow-2xl"
             alt="dentist chair"
           />
-          <div></div>
+          <div className="mr-6">
+            <DayPicker
+              mode="single"
+              selected={selectedDate}
+              onSelect={setSelectedDate}
+            ></DayPicker>
+          </div>
         </div>
       </div>
     </header>
