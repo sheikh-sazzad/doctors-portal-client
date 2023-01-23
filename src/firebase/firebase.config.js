2f6042bf85431db9e44482fa658dfc1ef5b04b28
sheikh-sazzad <sheikh15-11045@diu.edu.bd>
2023-01-23 07:02:34 +0600

    auth complete

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDyCcyvV3FH_hYbYm9lLt7R1VwIvHwiN0c",
+  authDomain: "doctors-portal-client-10170.firebaseapp.com",
+  projectId: "doctors-portal-client-10170",
+  storageBucket: "doctors-portal-client-10170.appspot.com",
+  messagingSenderId: "932293266975",
+  appId: "1:932293266975:web:cddade059b76dc99fbcf2a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
